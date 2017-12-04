b5c777654773be07463c4d1273b18003f7258e58
Murarius <murarius2011@gmail.com>
2017-12-04 13:25:08 +0100

    react and rspec init

---
+++ app/javascript/components/GameWindow.js
@@ -0,0 +1,9 @@
+import React from "react"
+import PropTypes from "prop-types"
+class GameWindow extends React.Component {
+  render () {
+    return <div>test</div>;
+  }
+}
+
+export default GameWindow
